2883cda51ef00f04dec25bcaf020131902a3f2c9
mattn <mattn.jp@gmail.com>
2013-08-19 16:13:59 +0900

    Fixes command parsing

--- etcdenv.go
+++ etcdenv.go
@@ -13,28 +13,41 @@ var key = flag.String("key", "", "etcd key")
 
 func main() {
 	flag.Parse()
-	if flag.NArg() == 0 || *key == "" {
+	if flag.NArg() > 0 && *key == "" {
 		fmt.Fprintln(os.Stderr, "etcdenv [-key=key] [...]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 	client := etcd.NewClient()
 	res, err := client.Get(*key)
-	if err == nil {
-		for _, n := range res {
-			key := strings.Split(n.Key, "/")
-			k, v := strings.ToUpper(key[len(key)-1]), n.Value
-			fmt.Println("creating env var:", k)
-			os.Setenv(k, v)
+
+	if flag.NArg() > 0 {
+		if err == nil {
+			for _, n := range res {
+				key := strings.Split(n.Key, "/")
+				k, v := strings.ToUpper(key[len(key)-1]), n.Value
+				fmt.Println("creating env var:", k)
+				os.Setenv(k, v)
+			}
 		}
-	}
-	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	err = cmd.Run()
-	if err != nil {
-		os.Exit(1)
+		cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		err = cmd.Run()
+		if err != nil {
+			os.Exit(1)
+		}
+	} else {
+		if err == nil {
+			for _, n := range res {
+				key := strings.Split(n.Key, "/")
+				k, v := strings.ToUpper(key[len(key)-1]), n.Value
+				fmt.Println("%s=%s", k, v)
+			}
+		} else {
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 	os.Exit(0)
 }
