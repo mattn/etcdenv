fc63797ebffa063b8da62cda96973ea1c2a40d92
mattn <mattn.jp@gmail.com>
2013-08-22 09:17:28 +0900

    Refactoring

--- etcdenv.go
+++ etcdenv.go
@@ -20,33 +20,31 @@ func main() {
 	}
 	client := etcd.NewClient()
 	res, err := client.Get(*key)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "etcdenv:", err)
+		os.Exit(1)
+	}
 
-	if flag.NArg() > 0 {
-		if err == nil {
-			for _, n := range res {
-				key := strings.Split(n.Key, "/")
-				k, v := strings.ToUpper(key[len(key)-1]), n.Value
-				fmt.Println("creating env var:", k)
-				os.Setenv(k, v)
-			}
+	envs := []string{}
+	for _, n := range res {
+		key := strings.Split(n.Key, "/")
+		k, v := strings.ToUpper(key[len(key)-1]), n.Value
+		envs = append(envs, k + "=" + v)
+	}
+	if flag.NArg() == 0 {
+		for _, env := range envs {
+			line := fmt.Sprintf("%q", env)
+			fmt.Println(line[1:len(line)-1])
 		}
-		cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-		cmd.Stdin = os.Stdin
-		err = cmd.Run()
-		if err != nil {
-			os.Exit(1)
-		}
-	} else {
-		if err == nil {
-			for _, n := range res {
-				key := strings.Split(n.Key, "/")
-				k, v := strings.ToUpper(key[len(key)-1]), n.Value
-				fmt.Printf("%s=%s\n", k, v)
-			}
-		} else {
-			fmt.Fprintln(os.Stderr, err)
-		}
+		os.Exit(0)
+	}
+	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
+	cmd.Env = envs
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	err = cmd.Run()
+	if err != nil {
+		os.Exit(1)
 	}
 }
