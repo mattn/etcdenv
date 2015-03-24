66cc597c6a1c1c10b29884fb3abae9a5c66b20e4
stuart.warren <stuart.warren@ocado.com>
2015-03-24 21:24:08 +0000

    Press ENTER or type command to continue Add extra recursive flag
    This adds an extra flag to go into fully recursive mode.
    It also joins the values of repeating keys together.
    
    eg:
    $ etcdctl set /appx/hosta/port_http hosta:80
    $ etcdctl set /appx/hosta/port_https hosta:443
    $ etcdctl set /appx/hostb/port_http hostb:80
    $ etcdenv -r -key /appx
    PORT_HTTP=hosta:80,hostb:80
    PORT_HTTPS=hosta:443
    
    Could be used with https://github.com/spotify/docker-cassandra to
    provide $CASSANDRA_SEEDS or similar

--- etcdenv.go
+++ etcdenv.go
@@ -3,17 +3,46 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/coreos/go-etcd/etcd"
 	"os"
 	"os/exec"
 	"strings"
 	"syscall"
+
+	"github.com/coreos/go-etcd/etcd"
 )
 
 var sep = flag.Bool("s", false, "separate arguments with spaces")
+var rec = flag.Bool("r", false, "recursively fetch child values")
 var key = flag.String("key", "", "etcd key")
 var host = flag.String("host", "", "etcd host")
 var hosts []string
+var envs []string
+
+func stringInSlice(s string, list []string) int {
+	for i, entry := range list {
+		if strings.Contains(entry, s) {
+			return i
+		}
+	}
+	return -1
+}
+
+func handleNode(n *etcd.Node) {
+	if !n.Dir {
+		key := strings.Split(n.Key, "/")
+		k, v := strings.ToUpper(key[len(key)-1]), n.Value
+		// if key already exists and in recursive mode, append with comma
+		if i := stringInSlice(k+"=", envs); i != -1 && *rec {
+			envs[i] = fmt.Sprint(envs[i], ",", v)
+		} else {
+			envs = append(envs, k+"="+v)
+		}
+	} else if *rec {
+		for _, n2 := range n.Nodes {
+			handleNode(n2)
+		}
+	}
+}
 
 func main() {
 	if len(os.Args) > 1 && strings.HasPrefix(os.Args[1], "-s ") {
@@ -32,7 +61,7 @@ func main() {
 		*host = os.Getenv("ETCDENV_HOST")
 	}
 	if *host != "" {
-		hosts = []string{*host}
+		hosts = strings.Split(*host, ",")
 	}
 	client := etcd.NewClient(hosts)
 	res, err := client.Get(*key, true, true)
@@ -41,13 +70,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	envs := os.Environ()
+	envs = os.Environ()
 	for _, n := range res.Node.Nodes {
-		if !n.Dir {
-			key := strings.Split(n.Key, "/")
-			k, v := strings.ToUpper(key[len(key)-1]), n.Value
-			envs = append(envs, k+"="+v)
-		}
+		handleNode(n)
 	}
 
 	if flag.NArg() == 0 {
